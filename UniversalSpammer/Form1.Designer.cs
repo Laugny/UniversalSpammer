98de3e5b8aed124099cd0ce88e4bce5da7e05bf8
Laugny <79530050+Laugny@users.noreply.github.com>
2021-04-29 23:32:48 +0200

    Add files via upload

---
+++ UniversalSpammer/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace UniversalSpammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMadeBy = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtThreads = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblidle = new System.Windows.Forms.Label();
+            this.lblSpamming = new System.Windows.Forms.Label();
+            this.tmrSpammer = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(89, 127);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(154, 108);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UniversalSpammer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Made by";
+            // 
+            // lblMadeBy
+            // 
+            this.lblMadeBy.AutoSize = true;
+            this.lblMadeBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMadeBy.Location = new System.Drawing.Point(75, 49);
+            this.lblMadeBy.Name = "lblMadeBy";
+            this.lblMadeBy.Size = new System.Drawing.Size(55, 17);
+            this.lblMadeBy.TabIndex = 3;
+            this.lblMadeBy.TabStop = true;
+            this.lblMadeBy.Text = "Laugny";
+            this.lblMadeBy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblMadeBy_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(184, 241);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(59, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(119, 241);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(59, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtThreads
+            // 
+            this.txtThreads.Location = new System.Drawing.Point(184, 270);
+            this.txtThreads.MaxLength = 4;
+            this.txtThreads.Name = "txtThreads";
+            this.txtThreads.Size = new System.Drawing.Size(59, 20);
+            this.txtThreads.TabIndex = 7;
+            this.txtThreads.Text = "200";
+            this.txtThreads.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtThreads_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(131, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Threads:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(243, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "ms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(86, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Status:";
+            // 
+            // lblidle
+            // 
+            this.lblidle.AutoSize = true;
+            this.lblidle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblidle.ForeColor = System.Drawing.Color.Red;
+            this.lblidle.Location = new System.Drawing.Point(127, 109);
+            this.lblidle.Name = "lblidle";
+            this.lblidle.Size = new System.Drawing.Size(27, 15);
+            this.lblidle.TabIndex = 11;
+            this.lblidle.Text = "idle";
+            // 
+            // lblSpamming
+            // 
+            this.lblSpamming.AutoSize = true;
+            this.lblSpamming.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpamming.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblSpamming.Location = new System.Drawing.Point(127, 109);
+            this.lblSpamming.Name = "lblSpamming";
+            this.lblSpamming.Size = new System.Drawing.Size(66, 15);
+            this.lblSpamming.TabIndex = 12;
+            this.lblSpamming.Text = "spamming";
+            this.lblSpamming.Visible = false;
+            // 
+            // tmrSpammer
+            // 
+            this.tmrSpammer.Tick += new System.EventHandler(this.tmrSpammer_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(24, 212);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 325);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblSpamming);
+            this.Controls.Add(this.lblidle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtThreads);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.lblMadeBy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UniversalSpammer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel lblMadeBy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtThreads;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblidle;
+        private System.Windows.Forms.Label lblSpamming;
+        private System.Windows.Forms.Timer tmrSpammer;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ UniversalSpammer/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UniversalSpammer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblMadeBy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.laugny.com/");
+        }
+
+        private void txtThreads_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if(!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblMadeBy.Enabled = false;
+            lblidle.Visible = false;
+            lblSpamming.Visible = true;
+            txtThreads.Enabled = false;
+            btnStart.Enabled = false;
+            txtMsg.Enabled = false;
+            btnClear.Enabled = false;
+            if (txtThreads.TextLength == 0)
+            {
+                MessageBox.Show("Please specify a number of Threads!","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                txtThreads.Enabled = true;
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                txtMsg.Enabled = true;
+                btnClear.Enabled = true;
+            }
+            else
+            {
+                tmrSpammer.Start();
+                Thread.Sleep(500);
+                btnStop.Enabled = true;
+            }
+        }
+
+        private void tmrSpammer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(Convert.ToInt32(txtThreads.Text));
+                SendKeys.Send(txtMsg.Text);
+                SendKeys.Send("{Enter}");
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmrSpammer.Stop();
+            lblSpamming.Visible = false;
+            lblidle.Visible = true;
+            txtThreads.Enabled = true;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            txtMsg.Enabled = true;
+            lblMadeBy.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMsg.Text = "";
+        }
+    }
+}
